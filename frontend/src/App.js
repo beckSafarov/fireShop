23591c6572517883c31a8cd18541601a1c66fac5
Begzod Safarov <begzod.safarov07@gmail.com>
2021-02-12 12:00:13 +0800

    started bootstrap designs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
 
 const  App = ()=>{
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+    <Header/>
+    <main>
+      <Container>
+        <h1>Welcome to Proshop</h1>
+      </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
