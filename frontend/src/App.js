d6f21e1b7ae1bb3f56f01a35893592ad8f8641bc
Begzod Safarov <begzod.safarov07@gmail.com>
2021-02-12 17:28:57 +0800

    basic design, homse screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 import {Container} from 'react-bootstrap'
 
 const  App = ()=>{
   return (
     <>
     <Header/>
-    <main>
+    <main className = 'py-3'>
       <Container>
-        <h1>Welcome to Proshop</h1>
+        <HomeScreen/>
       </Container>
     </main>
     <Footer/>
