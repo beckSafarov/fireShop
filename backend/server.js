5c3509c9c1e14f199df0e85867b3c093d80b7b1e
Begzod Safarov <begzod.safarov07@gmail.com>
2021-02-14 12:08:31 +0800

    common js to es6modules

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const products = require('./data/products')
-
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
 const app = express();
 
+dotenv.config();
 
 app.get('/', (req, res)=>{
     res.send('API is running');
@@ -36,6 +37,7 @@ app.get('/api/products/:id', (req, res)=>{
 });
 
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 
