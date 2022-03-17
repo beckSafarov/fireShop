96767b5cb0d455934c8fbfd7606a5535b7446d7d
Begzod Safarov <begzod.safarov07@gmail.com>
2022-03-17 09:08:55 +0800

    Admin reducers improved with immer

--- frontend/src/reducers/adminReducers.js
+++ frontend/src/reducers/adminReducers.js
@@ -1,104 +1,92 @@
 import * as cs from '../constants'
-const Loading = (state = {}, loading = true) => ({ ...state, loading })
-const Error = (error, state = {}) => ({ loading: false, error, ...state })
+import produce from 'immer'
 
-export const userListReducer = (state = { users: [] }, action) => {
-  let currList = state.users
-  let newState, newList
-
-  const Success = (users = action.payload) => ({
-    loading: false,
-    success: true,
-    users,
-  })
+export const userListReducer = produce((draft = { users: [] }, action) => {
   switch (action.type) {
     case cs.USER_LIST_REQUEST:
-      return Loading()
+      draft.loading = true
+      break
     case cs.USER_LIST_SUCCESS:
-      return Success()
+      return { ...draft, loading: false, success: true, users: action.payload }
     case cs.USER_LIST_FAILURE:
-      return Error(action.payload)
+      return { ...draft, loading: false, error: action.payload }
     case cs.USER_LIST_UPDATE:
-      const updatedUser = action.payload
-      for (let i = 0; i < currList.length; i++) {
-        if (currList[i]._id === updatedUser._id) {
-          currList[i] = updatedUser
-          break
-        }
-      }
-      return Success(currList)
+      draft.users = draft.users.map((user) =>
+        user._id === action.payload._id ? action.payload : user
+      )
+      draft.success = true
+      break
     case cs.USER_LIST_REMOVE:
-      newList = currList.filter((user) => user._id !== action.payload)
-      return Success(newList)
+      draft.users = draft.users.filter((user) => user._id !== action.payload)
+      draft.success = true
+      break
     case cs.USER_LIST_PROPERTY_RESET:
-      newState = state
-      newState[action.payload] = null
-      return newState
+      draft[action.payload] = null
+      break
     default:
-      return state
+      return draft
   }
-}
+})
 
-export const searchUserReducer = (state = {}, action) => {
+export const searchUserReducer = produce((draft = {}, action) => {
   switch (action.type) {
     case cs.ADMIN_SEARCH_USER_REQUEST:
-      return { loading: true }
+      return { ...draft, loading: true }
     case cs.ADMIN_SEARCH_USER_SUCCESS:
       return { loading: false, success: true, users: action.payload }
     case cs.ADMIN_SEARCH_USER_FAILURE:
       return { loading: false, error: action.payload }
     case cs.ADMIN_SEARCH_PROPERTY_RESET:
-      let newState = state
-      newState[action.payload] = null
-      return newState
+      draft[action.payload] = null
+      break
     case cs.ADMIN_SEARCH_USER_RESET:
       return {}
     default:
-      return state
+      return draft
   }
-}
+})
 
-export const adminUserDeleteReducer = (state = {}, action) => {
+export const adminUserDeleteReducer = produce((draft = {}, action) => {
   switch (action.type) {
     case cs.ADMIN_USER_DELETE_REQUEST:
-      return Loading()
+      return { ...draft, loading: true }
     case cs.ADMIN_USER_DELETE_SUCCESS:
       return { loading: false, success: true, message: action.payload }
     case cs.ADMIN_USER_DELETE_FAILURE:
-      return Error(action.payload)
+      return { ...draft, loading: false, error: action.payload }
     case cs.ADMIN_USER_DELETE_RESET:
       return {}
     default:
-      return state
+      return draft
   }
-}
+})
 
-export const adminUserUpdateReducer = (state = {}, action) => {
+export const adminUserUpdateReducer = produce((draft = {}, action) => {
   switch (action.type) {
     case cs.ADMIN_USER_UPDATE_REQUEST:
-      return Loading(state)
+      return { ...draft, loading: true }
     case cs.ADMIN_USER_UPDATE_SUCCESS:
       return { loading: false, success: true, user: action.payload }
     case cs.ADMIN_USER_UPDATE_FAILURE:
-      return Error(action.payload, state)
+      return { ...draft, loading: false, error: action.payload }
     case cs.ADMIN_USER_UPDATE_RESET:
       return {}
     default:
-      return state
+      return draft
   }
-}
+})
 
-export const imgUploadReducer = (state = {}, action) => {
+export const imgUploadReducer = produce((draft = {}, action) => {
   switch (action.type) {
     case cs.IMG_UPLOAD_REQUEST:
-      return Loading(state)
+      return { ...draft, loading: true }
     case cs.IMG_UPLOAD_SUCCESS:
       return { loading: false, success: true, data: action.payload }
     case cs.IMG_UPLOAD_FAILURE:
-      return Error(action.payload)
+      return { ...draft, error: action.payload }
     case cs.IMG_UPLOAD_RESET:
       return {}
     default:
-      return state
+      return draft
   }
-}
+})
