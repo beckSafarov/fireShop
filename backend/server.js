f879b4448e85677404b528a670bd6b99d731c948
Begzod Safarov <begzod.safarov07@gmail.com>
2022-08-16 09:45:00 +0800

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
-import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import cartRoutes from './routes/cartRoutes.js'
@@ -12,18 +11,15 @@ import adminRoutes from './routes/adminRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import morgan from 'morgan'
-
+const nodeEnv = process.env.NODE_ENV
 //function declarations
 dotenv.config()
 connectDB()
 const app = express()
-process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
+nodeEnv === 'development' && app.use(morgan('dev'))
 app.use(express.json())
 
 //routes
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
 
 const __dirname = path.resolve()
 app.use('/api/uploads', express.static(path.join(__dirname, '/uploads')))
@@ -40,6 +36,15 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+if (nodeEnv === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => res.send('API is running'))
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
@@ -48,8 +53,7 @@ const PORT = process.env.PORT || 5000
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-      .underline.bold
+    `Server running in ${nodeEnv} mode on port ${PORT}`.yellow.underline.bold
   )
 )
 
